95436a3a3030bd44b7159cc5f5097e1d603dc7ed
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2024-05-08 13:16:44 +0000

    first commit

---
+++ frontend/src/app/login/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>asjkdhasjkdhasjkbdsjK</div>
+  )
+}
+
+export default page
---
+++ frontend/src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Link href="/login">
+        <p>sabias
+        </p>
+      </Link>
+    </main>
+  );
+}
