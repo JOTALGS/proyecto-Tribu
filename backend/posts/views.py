a48fbfa8507b98f2eb3346a92f3e0bf781c3a27e
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-06-04 16:08:55 -0300

    Update views.py

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post, LikePost
 
-# Create your views here.
+def upload(request):
+
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/')
