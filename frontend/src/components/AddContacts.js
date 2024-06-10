2f0af0e4314d7ef197dbc80c4337a97eccb31885
NicoV00 <6328@holbertonstudents.com>
2024-06-10 19:35:25 +0000

    add contacts widget

--- frontend/src/components/AddContacts.js
+++ frontend/src/components/AddContacts.js
@@ -1,12 +1,7 @@
-'use client'
 import React from 'react';
 import { MDBContainer, MDBTable, MDBTableHead, MDBTableBody, MDBBtn } from 'mdb-react-ui-kit';
 
-
-
 function App() {
-
-  
   return (
     <div>
       <style jsx>{`
@@ -22,37 +17,37 @@ function App() {
       `}</style>
       <MDBContainer className='' fluid>
         <section>
-          <div className='shadow-8 rounded-4 overflow-hidden ml-auto'  style={{ width: '20vw', height: '60vh' }}>
+          <div className='shadow-8 rounded-4 overflow-hidden ml-auto'  style={{ width: '85%', maxWidth: '350px', height: '30vh' }}>
             <MDBTable>
               <MDBTableHead light>
                 <tr>
-                  <th style={{ fontSize: '1em', fontWeight: 'bold' }}>Recommended users</th>
+                  <th>Work with</th>
                   <th>Role</th>
                 </tr>
               </MDBTableHead>
               <MDBTableBody style={{ verticalAlign: 'middle' }}>
-              <a href='/profile'>
                 <tr>
                   <td>
-                    <div className='d-flex align-items-center'>
-                      <img
-                        src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                        alt=''
-                        style={{ width: '45px', height: '45px' }}
-                        className='rounded-circle'
-                      />
-                      <div className='ms-3'>
-                        <p className='fw-bold mb-1'>John Doe</p>
-                        <p className='text-muted mb-0'>john.doe@gmail.com</p>
+                    <a href='/profile' style={{ textDecoration: 'none', color: 'inherit' }}>
+                      <div className='d-flex align-items-center'>
+                        <img
+                          src='https://mdbootstrap.com/img/new/avatars/8.jpg'
+                          alt=''
+                          style={{ width: '45px', height: '45px' }}
+                          className='rounded-circle'
+                        />
+                        <div className='ms-3'>
+                          <p className='fw-bold mb-1'>John Doe</p>
+                          <p className='text-muted mb-0'>jd@gmail.com</p>
+                        </div>
                       </div>
-                    </div>
+                    </a>
                   </td>
                   <td>
                     <p className='fw-normal mb-1'>Producer</p>
                     <p className='text-muted mb-0'>Beats</p>
                   </td>
                 </tr>
-                </a>
               </MDBTableBody>
             </MDBTable>
           </div>
