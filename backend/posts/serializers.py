b88beb010561e497857a947353d8fc25ce3fb248
jotalgs <jose.pgilsuarez@gmail.com>
2024-06-11 18:11:51 +0000

    Guardar cambios locales

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from .models import Post
+from django.contrib.auth.models import User
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
 
 class PostSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer(read_only=True)
+
     class Meta:
         model = Post
         fields = '__all__'
