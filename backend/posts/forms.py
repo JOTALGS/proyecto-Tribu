ebcfbd7e1b72e465c469be0deb6058955aeead97
jotalgs <jose.pgilsuarez@gmail.com>
2024-06-11 21:17:31 +0000

    styles modal

--- backend/posts/forms.py
+++ backend/posts/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('created_by', 'body')
+        fields = ('created_by', 'body', 'link',)
