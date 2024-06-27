0cbbbe8de94519f3263629a403da293e99e38b31
Nicolás Valles <6328@holbertonstudents.com>
2024-06-27 19:10:06 +0000

    changed register form styles

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -3,7 +3,41 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import api from '../utils/api';
 import { useAuth } from '../stores/user';
-import { Button, Form } from 'react-bootstrap';
+import styled from 'styled-components';
+import Link from 'next/link';
+
+const VideoContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const Video = styled.video`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+`;
+
+const FormContainer = styled.div`
+  width: 100%;
+  max-width: 400px;
+  padding: 20px;
+  background-color: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(10px);
+  border-radius: 8px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  margin: 0 auto; /* Center the form container horizontally */
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
 
 const Register = () => {
   const [username, setUsername] = useState('');
@@ -16,7 +50,6 @@ const Register = () => {
   const { saveAuthTokens } = useAuth();
 
   const onButtonClick = async () => {
-    console.log('Register clicked');
     const body = {
       username,
       email,
@@ -24,21 +57,12 @@ const Register = () => {
       password2,
     };
     try {
-      const response = await api.post('api/signup/', body, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      console.log('Response:', response.data);
+      const response = await api.post('api/signup/', JSON.stringify(body));
       if (response.data) {
         saveAuthTokens(response.data);
-        console.log('Registration successful');
         router.push('/home');
-      } else {
-        console.log('No data received');
       }
     } catch (error) {
-      console.log('Error:', error);
       if (error.response && error.response.data) {
         if (error.response.data.email) setEmailError(error.response.data.email);
         if (error.response.data.password1 || error.response.data.password2) {
@@ -49,55 +73,74 @@ const Register = () => {
       }
     }
   };
-  
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-md rounded-lg">
-        <h2 className="text-2xl font-bold text-center text-gray-900">Register</h2>
-        <div className="space-y-4">
-          <div>
-            <input
-              value={username}
-              onChange={(ev) => setUsername(ev.target.value)}
-              placeholder="Enter your username here"
-              className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+    <div className="flex flex-col items-center justify-center min-h-screen relative">
+      <VideoContainer>
+        <Video src="/videos/tribu.mp4" type="video/mp4" autoPlay muted loop />
+      </VideoContainer>
+      <div className="w-full max-w-md rounded-lg mb-32 relative z-10">
+        <LogoContainer>
+          <Link href="/home">
+            <img
+              src="/images/logo.png"
+              alt="Tribu logo"
+              style={{ cursor: 'pointer', width: '360px', height: '140px' }}
             />
+          </Link>
+        </LogoContainer>
+        <FormContainer>
+          <h2 className="text-2xl font-bold text-center text-gray-900 mb-6">Register</h2>
+          <div className="space-y-4">
+            <div>
+              <input
+                value={username}
+                onChange={(ev) => setUsername(ev.target.value)}
+                placeholder="Enter your username here"
+                className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-rose-700"
+              />
+            </div>
+            <div>
+              <input
+                value={email}
+                onChange={(ev) => setEmail(ev.target.value)}
+                placeholder="Enter your email here"
+                className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-rose-700"
+              />
+              {emailError && <label className="text-red-500 text-sm">{emailError}</label>}
+            </div>
+            <div>
+              <input
+                value={password1}
+                onChange={(ev) => setPassword1(ev.target.value)}
+                type="password"
+                placeholder="Enter your password here"
+                className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-rose-700"
+              />
+            </div>
+            <div>
+              <input
+                value={password2}
+                onChange={(ev) => setPassword2(ev.target.value)}
+                type="password"
+                placeholder="Confirm your password here"
+                className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-rose-700"
+              />
+              {passwordError && <label className="text-red-500 text-sm">{passwordError}</label>}
+            </div>
+            <div>
+              <button
+                onClick={onButtonClick}
+                className="w-full px-4 py-2 font-bold text-white rounded-lg 
+                bg-gray-900 transition-all duration-900 
+                hover:bg-gradient-to-r hover:from-pink-500 hover:via-pink-900 hover:to-purple-800
+                focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+              >
+                Go to TRIBU
+              </button>
+            </div>
           </div>
-          <div>
-            <input
-              value={email}
-              onChange={(ev) => setEmail(ev.target.value)}
-              placeholder="Enter your email here"
-              className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-            />
-            {emailError && <label className="text-red-500 text-sm">{emailError}</label>}
-          </div>
-          <div>
-            <input
-              value={password1}
-              onChange={(ev) => setPassword1(ev.target.value)}
-              type="password"
-              placeholder="Enter your password here"
-              className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-            />
-          </div>
-          <div>
-            <input
-              value={password2}
-              onChange={(ev) => setPassword2(ev.target.value)}
-              type="password"
-              placeholder="Confirm your password here"
-              className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-            />
-            {passwordError && <label className="text-red-500 text-sm">{passwordError}</label>}
-          </div>
-          <div>
-          <Button onClick={onButtonClick}  variant="success" className="py-2 px-4 rounded-md">Go to TRIBU</Button>
-              
-      
-          </div>
-        </div>
+        </FormContainer>
       </div>
     </div>
   );
